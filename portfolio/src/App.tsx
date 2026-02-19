ead9ab2a58b58da9101733783f5393316e4bf6a2
Josekariz <sejokarizz@gmail.com>
2026-02-19 16:36:05 +0300

    Refactor routing and component structure in App; update package dependencies

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -1,14 +1,34 @@
-import { Routes, Route } from "react-router-dom"
-import Projects from "./projects/Projects"
-import ProjectDetails from "./projects/ProjectDetails"
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Homepage from "./home/Homepage";
+import Services from "./services/Services";
+import About from "./about/About";
+import Projects from "./projects/Projects";
+import Blog from "./blog/Blog";
+import BlogPost from "./blog/Blogpost";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Projects />} />
-      <Route path="/projects/:id" element={<ProjectDetails />} />
-    </Routes>
-  )
+    <>
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/projects/:id" element={<ProjectDetails />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/blog/:slug" element={<BlogPost />} />
+        <Route path="/about" element={<About />} />
+
+        <Route path="/404" element={<NotFound />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+
+      <Footer />
+    </>
+  );
 }
 
-export default App
+export default App;
