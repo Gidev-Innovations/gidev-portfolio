8597a82d55e5aede07b359489ae1790a348389fd
Josekariz <sejokarizz@gmail.com>
2026-02-28 11:45:04 +0300

    fix: clean up App component structure and add contact route

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -10,37 +10,39 @@ import BlogPost from "./blog/Blogpost";
 import ProjectDetails from "./projects/ProjectDetails";
 import NotFound from "./not-found/NotFound";
 import { ArrowUpFromDot } from "lucide-react";
+import Contact from "./contact/Contact";
 
 function App() {
   return (
-    <div className="relative">
-      <Navbar />
+    <>
+      <div className="relative">
+        <Navbar />
 
-            <Routes>
-                <Route path="/" element={<Homepage />} />
-                <Route path="/projects" element={<Projects />} />
-                <Route path="/projects/:id" element={<ProjectDetails />} />
-                <Route path="/services" element={<Services />} />
-                <Route path="/blog" element={<Blog />} />
-                <Route path="/blog/:slug" element={<BlogPost />} />
-                <Route path="/about" element={<About />} />
-
-                <Route path="/404" element={<NotFound />} />
-                <Route path="*" element={<NotFound />} />
-            </Routes>
-        <div className="animate-bounce absolute bottom-4 right-4 cursor-pointer" onClick={() => window.scrollTo({top: 0, behavior: 'smooth'})}>
-           <div className="h-8 w-8 rounded-full bg-secondary-950 flex items-center justify-center">
-               <ArrowUpFromDot color="#18aeb4" />
-           </div>
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/projects/:id" element={<ProjectDetails />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/blog" element={<Blog />} />
+          <Route path="/blog/:slug" element={<BlogPost />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/404" element={<NotFound />} />
+          <Route path="*" element={<NotFound />} />
+          <Route path="/404" element={<NotFound />} />
+          <Route path="*" element={<NotFound />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+        <Footer />
+        <div
+          className="animate-bounce absolute bottom-4 right-4 cursor-pointer"
+          onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
+        >
+          <div className="h-8 w-8 rounded-full bg-secondary-950 flex items-center justify-center">
+            <ArrowUpFromDot color="#18aeb4" />
+          </div>
         </div>
-
-        <Route path="/404" element={<NotFound />} />
-        <Route path="*" element={<NotFound />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
-
-      <Footer />
-    </div>
+      </div>
+    </>
   );
 }
 
